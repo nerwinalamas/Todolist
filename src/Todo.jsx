562f865d51ac9f4db307f971a7b4eef5eeb650c8
unknown <nerwinalamas@gmail.com>
2023-07-01 18:03:05 +0800

    Set fix height in tasklist, Adding Total Tasks

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,86 +1,105 @@
 import React, { useState } from "react";
 import { Data } from "./data";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { FaTrash, FaCheck } from "react-icons/fa";
 
 const Todo = () => {
   const [data, setData] = useState(Data);
   const [title, setTitle] = useState("");
-  const [toggleDone, setToggleDone] = useState(false);
 
-  const handleAddClick = () => {
-    if (title === "") return false
-    setData([
-      ...data,
-      {
-        id: uuidv4(),
-        title: title,
-        done: false
-      },
-    ]);
-    setTitle("");
+  const handleAdd = () => {
+    if (title) {
+      setData([
+        ...data,
+        {
+          id: uuidv4(),
+          title: title,
+          done: false,
+        },
+      ]);
+      setTitle("");
+    }
   };
 
-  const handleDeleteClick = (dataId) => {
+  const handleDelete = (dataId) => {
     setData(data.filter((d) => d.id !== dataId));
   };
 
-  const handleDoneClick = (d) => {
-    setData(data.map(todo => {
-      if (todo.id === d.id) {
-        return {...todo, done: !todo.done}
-      } else {
-        return todo
-      }
-    }))
+  const handleToggleDone = (d) => {
+    setData(
+      data.map((todo) => {
+        if (todo.id === d.id) {
+          return { ...todo, done: !todo.done };
+        } else {
+          return todo;
+        }
+      })
+    );
   };
 
   return (
-    <div className="w-full h-[500px] md:w-96 bg-cardBackgroundColor p-2 rounded-md">
-      <div className="">
-        {/* CREATE */}
-        <div className="flex justify-between mb-5">
-          <input 
-            type="text"
-            required
-            placeholder="Task for today"
-            className="w-full mr-2 rounded-md p-2"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <button
-          className="text-cardBackgroundColor" 
-          onClick={handleAddClick}
-          disabled={!title}
-          >ADD</button>
-        </div>
+    <>
+      <div className="w-full h-[600px] md:w-96 bg-cardBackgroundColor rounded-md">
+        <div className="h-full relative">
+          {/* CREATE */}
+          <div className="flex justify-between mb-3">
+            <input
+              type="text"
+              required
+              placeholder="Task for today"
+              className="w-full m-3 rounded-md p-2"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <button
+              className="text-cardBackgroundColor my-3 mr-3"
+              onClick={handleAdd}
+              disabled={!title}
+            >
+              ADD
+            </button>
+          </div>
 
-        {/* GET ALL / Todo */}
-        <div className="max-h-[410px] text-white overflow-y-auto">
-          {data.map((d) => {
-            return (
-              <div key={d.id} className="flex justify-between m-2 p-2 ">
-                <span className={d.done ? "line-through decoration-red-500 decoration-2" : "no-underline"}>{d.title}</span>
-                <div className="flex">
-                  <a
-                    onClick={() => handleDoneClick(d)}
-                    className=" text-white mx-1"
+          {/* GET ALL / Todo */}
+          <div className="h-[400px] text-cardBackgroundColor font-medium overflow-y-auto overscroll-y-none bg-white mx-3 rounded">
+            {data.map((d) => {
+              return (
+                <div key={d.id} className="flex justify-between m-2 p-2 ">
+                  <span
+                    className={
+                      d.done
+                        ? "line-through decoration-red-500 decoration-2"
+                        : "no-underline"
+                    }
                   >
-                    <FaCheck />
-                  </a>
-                  <a
-                    onClick={() => handleDeleteClick(d.id)}
-                    className=" text-white mx-1"
-                  >
-                    <FaTrash />
-                  </a>
+                    {d.title}
+                  </span>
+                  <div className="flex">
+                    <a
+                      onClick={() => handleToggleDone(d)}
+                      className=" text-cardBackgroundColor mx-1"
+                    >
+                      <FaCheck />
+                    </a>
+                    <a
+                      onClick={() => handleDelete(d.id)}
+                      className=" text-cardBackgroundColor mx-1"
+                    >
+                      <FaTrash />
+                    </a>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+          <div className="w-[80px] h-[40px] bg-white font-medium ml-3 my-3 flex items-center justify-center p-2 rounded">
+            <span className="text-cardBackgroundColor">
+              {data.length} Tasks
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 export const Data = 
 [
     {id: 0, title: "Breakfast", done: true},
-    {id: 1, title: "Meds", done: true},
+    {id: 1, title: "Meds", done: false},
     {id: 2, title: "Therapy", done: false},
-    {id: 3, title: "Code", done: false},
+    {id: 3, title: "Code", done: true},
     {id: 4, title: "Lunch", done: false}
 ]
